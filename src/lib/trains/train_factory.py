4369c58a98a1e69641d06f066684367e55791c66
Regan <andrew.regan@capgemini.com>
2022-05-31 17:26:23 +1000

    commented out module imports for modules that don't exist

--- src/lib/trains/train_factory.py
+++ src/lib/trains/train_factory.py
@@ -4,24 +4,24 @@ from __future__ import print_function
 
 from .ctdet import CtdetTrainer
 from .ctdetSpotNet2 import CtdetTrainerSpotNet2
-from .ctdetMultiSpot import CtdetTrainerMultiSpot
+#from .ctdetMultiSpot import CtdetTrainerMultiSpot
 from .ctdetVid import CtdetTrainerVid
 from .ctdetSpotNetVid import CtdetTrainerSpotNetVid
-from .ctdetWipeNet import CtdetWipeNetTrainer
-from .ddd import DddTrainer
-from .exdet import ExdetTrainer
-from .multi_pose import MultiPoseTrainer
+#from .ctdetWipeNet import CtdetWipeNetTrainer
+#from .ddd import DddTrainer
+#from .exdet import ExdetTrainer
+#from .multi_pose import MultiPoseTrainer
 
 
 train_factory = {
-  'exdet': ExdetTrainer, 
-  'ddd': DddTrainer,
+#  'exdet': ExdetTrainer, 
+#  'ddd': DddTrainer,
   'ctdet': CtdetTrainer,
   'ctdetSpotNet2': CtdetTrainerSpotNet2,
-  'ctdetMultiSpot': CtdetTrainerMultiSpot,
+#  'ctdetMultiSpot': CtdetTrainerMultiSpot,
   'ctdetSpotNetVid': CtdetTrainerSpotNetVid,
   'ctdetVid': CtdetTrainerVid,
-  'multi_pose': MultiPoseTrainer, 
-  'ctdetWipeNet': CtdetWipeNetTrainer,
+#  'multi_pose': MultiPoseTrainer, 
+#  'ctdetWipeNet': CtdetWipeNetTrainer,
   
 }
